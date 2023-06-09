cf861138d08d40d76a769fd10f822582fdd2e5e4
dangtung <language0512@gmail.com>
2023-06-09 16:19:22 +0700

    update useeffect example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import CountTitle from './components/CountTitle';
 function App() {
-  return (
-    <div className="App">
-      this is react app
-    </div>
-  );
+    return (
+        <div className="App">
+            <CountTitle />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/CountTitle.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+const CountTitle = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("11111111");
+        document.title = `You have clicked ${count} times`;
+    });
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>Increase</button>
+        </div>
+    )
+}
+
+export default CountTitle;
